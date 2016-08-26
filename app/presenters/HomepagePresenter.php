c52056d22cafaa34a190cdbf0ce83b54e02a91dc
Jan Demel <Jan Demel>
2016-08-26 23:12:46 +0200

    Added intro box and skills section

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -8,10 +8,6 @@ use App\Model;
 
 class HomepagePresenter extends BasePresenter
 {
-
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
-	}
-
+    public function renderDefault(){
+    }
 }
