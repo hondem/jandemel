3e99ab47ae8af75679309bc4aeefc6805a8e0811
Jan Demel <Jan Demel>
2016-08-31 17:49:12 +0200

    Web update

---
+++ www/js/parallax.js
@@ -0,0 +1,18 @@
+$(window).scroll(function(){
+    var offset = $(window).scrollTop();
+    $('.intro').css({
+        'background-position':'0px ' + (offset / 4) + 'px'
+    });
+    
+    $('.intro .name, .intro .appendix').css({
+        'top': (50 + offset/12) + '%'
+    });
+    
+    if(offset > $('.parallax1').offset().top - $(window).height()){
+        offset = Math.min(2340, offset - $('.parallax1').offset().top + $(window).height());
+        console.log(offset);
+        $('.p1_l1').css({
+            'transform':'translate(0px,'+ -(offset/4) +'px)'
+        });
+    }
+});
